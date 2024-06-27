31a478130d2e3c3848110133114c3c0251a2ecd1
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2024-06-28 01:21:52 +0900

    chore: Add user routes to the API

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import setupSwagger from './config/swagger';
 
+import userRoutes from "./routes/userRoutes";
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 setupSwagger(app);
+
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to OtePal API!");
