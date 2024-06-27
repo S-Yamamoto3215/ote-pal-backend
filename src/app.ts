8c73fb394176f30551aa04f786589ad4b6bdeab3
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2024-06-27 23:00:26 +0900

    feat: Add Swagger documentation support

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import setupSwagger from './config/swagger';
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+setupSwagger(app);
 
 app.get("/", (req, res) => {
   res.send("Welcome to OtePal API!");
