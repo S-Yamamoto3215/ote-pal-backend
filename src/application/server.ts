575cd273c17b24aca823959a6cc0512599bb8fb3
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2024-08-06 11:18:43 +0900

    Update package-lock.json and nodemon.json

---
+++ src/application/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan("dev"));
+
+// Add your routes here
+app.get("/", (req, res) => {
+  res.send("Welcome to OtePal API!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
