856bf27e587143b189d14a247ce98e9f610617f8
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2024-09-19 22:34:46 +0900

    TypeORMの初期設定、DB接続の確認

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import "reflect-metadata";
 import app from "./infrastructure/http/express";
+import { AppDataSource } from "./infrastructure/database/dataSource";
 
 const PORT = process.env.PORT || 3000;
 
-// サーバーの起動
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Connected to the database");
+
+    // サーバーの起動
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.log("Database connection error: ", error));
