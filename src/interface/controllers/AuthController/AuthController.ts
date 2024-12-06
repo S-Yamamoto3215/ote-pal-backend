e10cd064f555d2966debe2fd296469d7ebd6250f
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2024-12-07 00:06:04 +0900

    Add AuthController

---
+++ src/interface/controllers/AuthController/AuthController.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+
+import { IAuthUseCase } from "@/application/usecases/AuthUseCase";
+import { IAuthController } from "@/interface/controllers/AuthController";
+
+export class AuthController implements IAuthController {
+  constructor(private authUseCase: IAuthUseCase) {}
+
+  async login(req: Request, res: Response, next: NextFunction): Promise<void> {
+    const { email, password } = req.body;
+
+    try {
+      const token = await this.authUseCase.login(email, password);
+      res.json({ token });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
