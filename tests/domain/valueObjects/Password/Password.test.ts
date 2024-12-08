b5bb1cd1fed5aead3562a3bfd6bc580334d69101
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2024-12-09 01:41:36 +0900

    Password Object auto hased

--- tests/domain/valueObjects/Password/Password.test.ts
+++ tests/domain/valueObjects/Password/Password.test.ts
@@ -3,7 +3,6 @@ import { Password } from "@/domain/valueObjects/Password";
 describe("Password", () => {
   it("should hash a password correctly", async () => {
     const password = new Password("password");
-    await password.hash();
     expect(password.getIsHashed()).toBe(true);
   });
 
@@ -23,22 +22,13 @@ describe("Password", () => {
     );
   });
 
-  it("should throw an error when comparing non-hashed passwords", async () => {
-    const password = new Password("password");
-    await expect(password.compare("password")).rejects.toThrow(
-      "Cannot compare a non-hashed password",
-    );
-  });
-
   it("should correctly compare a hashed password with a plain one", async () => {
     const password = new Password("password");
-    await password.hash();
     expect(await password.compare("password")).toBe(true);
   });
 
   it("should return false when comparing a hashed password with an incorrect plain one", async () => {
     const password = new Password("password");
-    await password.hash();
     expect(await password.compare("test")).toBe(false);
   });
 
--- tests/domain/valueObjects/Password/PasswordTransformer.test.ts
+++ tests/domain/valueObjects/Password/PasswordTransformer.test.ts
@@ -1,14 +1,23 @@
-import { Password, PasswordTransformer } from "@/domain/valueObjects/Password";
+import { PasswordTransformer } from "@/domain/valueObjects/Password/PasswordTransformer";
+import { Password } from "@/domain/valueObjects/Password";
 
 describe("PasswordTransformer", () => {
   it("should transform a Password to a string", () => {
-    const password = new Password("password");
+    const plainPassword = "password123";
+    const password = new Password(plainPassword);
     const transformer = new PasswordTransformer();
-    expect(transformer.to(password)).toBe("password");
+
+    const transformedValue = transformer.to(password);
+    expect(transformedValue).toBe(password.getValue());
   });
 
   it("should transform a string to a Password", () => {
+    const hashedPassword =
+      "$2b$10$gqwxWb.WDf6xnadzEU.CL.qGK/I5iynXbyjNIlduOuJFlSyTdWWNi";
     const transformer = new PasswordTransformer();
-    expect(transformer.from("password")).toBeInstanceOf(Password);
+
+    const password = transformer.from(hashedPassword);
+    expect(password.getValue()).toBe(hashedPassword);
+    expect(password.getIsHashed()).toBe(true);
   });
 });
