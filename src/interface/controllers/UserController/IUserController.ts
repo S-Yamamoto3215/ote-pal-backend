f4971258248cbe6f962443057fb7e9e61744e484
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2025-04-20 15:44:16 +0900

    Add UserController methods

--- src/interface/controllers/UserController/IUserController.ts
+++ src/interface/controllers/UserController/IUserController.ts
@@ -2,4 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 export interface IUserController {
   createUser(req: Request, res: Response, next: NextFunction): Promise<void>;
+  registerUser(req: Request, res: Response, next: NextFunction): Promise<void>;
+  verifyEmail(req: Request, res: Response, next: NextFunction): Promise<void>;
+  resendVerificationEmail(req: Request, res: Response, next: NextFunction): Promise<void>;
 }
--- src/interface/controllers/UserController/UserController.ts
+++ src/interface/controllers/UserController/UserController.ts
@@ -36,4 +36,76 @@ export class UserController implements IUserController {
       next(error);
     }
   }
+
+  async registerUser(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return next(
+        new AppError(
+          "ValidationError",
+          "Missing required fields: name, email, password"
+        )
+      );
+    }
+
+    try {
+      const user = await this.userUseCase.registerUser({
+        name,
+        email,
+        password,
+      })
+      res.status(201).json(user.id);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async verifyEmail(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    const { token } = req.body;
+
+    if (!token || typeof token !== "string") {
+      return next(new AppError("ValidationError", "Token is required"));
+    }
+
+    try {
+      await this.userUseCase.verifyEmail(token);
+      res.status(200).json({
+        message: "Email verified successfully"
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async resendVerificationEmail(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    const { email } = req.body;
+
+    if (!email) {
+      return next(
+        new AppError("ValidationError", "Missing required field: email")
+      );
+    }
+
+    try {
+      await this.userUseCase.resendVerificationEmail(email);
+      res.status(200).json({
+        message: "Verification email resent successfully",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
